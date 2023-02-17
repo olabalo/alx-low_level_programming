ef54609f79eefca0037ad3cb7a6535a8d8fd22f8
olabalo <alfredwinstonk@gmail.com>
2023-02-16 16:52:13 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return: Always 1
+ * main - Entry
+ * Return: 1
  */
 int main(void)
 {
-	write(i2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
